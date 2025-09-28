ac92ac35de8a0b64f47395ed7540b28dd774c3cd
Solentra <soulxz2007@gmail.com>
2025-09-28 09:24:51 -0500

    Update library.lua

--- library.lua
+++ library.lua
@@ -37,7 +37,7 @@ CommandEntry.BackgroundColor3 = Color3.new(1, 1, 1)
 CommandEntry.BackgroundTransparency = 0.67
 CommandEntry.BorderColor3 = Color3.new(0, 0, 0)
 CommandEntry.BorderSizePixel = 2
-CommandEntry.Position = UDim2.new(0.6, 0, 0.00672043022, 0)
+CommandEntry.Position = UDim2.new(0.734343195, 0, 0.00672043022, 0)
 CommandEntry.Size = UDim2.new(0, 267, 0, 38)
 
 InputBox.Name = "InputBox"
